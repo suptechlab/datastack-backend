8d00de8939e951da992c2945fe0aa91ed09e573f
Kevin Rejko <krejko@gmail.com>
2020-05-07 20:42:46 -0600

    Wrapped app in main block to catch errors, made /bigquery_tokens async, moved response into callback from auth function so it doesn't return too early

--- app.js
+++ app.js
@@ -5,36 +5,41 @@ const bodyparser = require("body-parser");
 
 const port = process.env.PORT || 3200;
 
-const {JWT} = require('google-auth-library');
+const { JWT } = require('google-auth-library');
 const keys = require('./conf/bigquery.json');
 
 // middleware
+async function main() {
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: false }));
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: false }));
 
-app.listen(port, () => {
-  console.log(`running at port ${port}`);
-});
-
-json_resp = {}
-
-app.options("/bigquery_tokens", cors())
-app.get("/bigquery_tokens", cors(), (req, res) => {
-  const client = new JWT(
-    keys.client_email,
-    null,
-    keys.private_key,
-    ['https://www.googleapis.com/auth/cloud-platform'],
-  );
-
-  client.authorize((err, response) => {
-    if (err) {
-      json_resp['error'] = err
-    } else {
-      json_resp['access_token'] = response.access_token;
-    }
+  app.listen(port, () => {
+    console.log(`running at port ${port}`);
   });
 
-  res.status(200).json(json_resp);
-});
+  json_resp = {}
+
+  app.options("/bigquery_tokens", cors())
+  app.get("/bigquery_tokens", cors(), async (req, res) => {
+
+    const client = new JWT(
+      keys.client_email,
+      null,
+      keys.private_key,
+      ['https://www.googleapis.com/auth/cloud-platform'],
+    );
+
+    await client.authorize((err, response) => {
+      if (err) {
+        json_resp['error'] = err
+      } else {
+        json_resp['access_token'] = response.access_token;
+      }
+      res.status(200).json(json_resp);
+    });
+  });
+
+}
+
+main().catch(console.error);
